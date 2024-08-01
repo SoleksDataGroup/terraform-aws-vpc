bda5d3167aac58919deec0f4b99fc2de9fef4f80
Daniel Mezentsev <dan@soleks.com>
2024-08-01 13:44:02 -0700

    fixed route settings for default table

--- main.tf
+++ main.tf
@@ -109,11 +109,9 @@ resource "aws_default_route_table" "vpc-defaul-route-table" {
       destination_prefix_list_id = lookup(route.value, "destination_prefix_list_id", null)
 
 // One of the following target arguments must be supplied:
-      carrier_gateway_id = lookup(route.value, "carrier_gateway_id", null)
       core_network_arn = lookup(route.value, "core_network_arn", null)
       egress_only_gateway_id = lookup(route.value, "egress_only_gateway_id", null)
       gateway_id = lookup(route.value, "gateway_id", null)
-      local_gateway_id =  lookup(route.value, "local_gateway_id", null)
       nat_gateway_id = lookup(route.value, "nat_gateway_id", null)
       network_interface_id = lookup(route.value, "network_interface_id", mull)
       transit_gateway_id = lookup(route.value, "transit_gateway_id", null)
